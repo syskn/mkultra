c671444c7aadaeee9895a8cb869ca25f2e3e05ff
Sta <bit192@gmail.com>
2021-11-24 06:46:42 +0900

    Update tokenizers.py

--- mkultra/tokenizers.py
+++ mkultra/tokenizers.py
@@ -1,4 +1,4 @@
-from transformers import GPT2Tokenizer, GPT2TokenizerFast
+from transformers import GPT2Tokenizer, GPT2TokenizerFast, T5Tokenizer
 from mkultra.soft_prompt import SoftPrompt
 
 class GPT2SPTokenizerFast(GPT2TokenizerFast):
@@ -26,6 +26,31 @@ class GPT2SPTokenizerFast(GPT2TokenizerFast):
         SoftPrompt._register_tokenizer(self)
 
 class GPT2SPTokenizer(GPT2Tokenizer):
+    def __init__(
+        self,
+        vocab_file,
+        merges_file,
+        tokenizer_file=None,
+        unk_token="<|endoftext|>",
+        bos_token="<|endoftext|>",
+        eos_token="<|endoftext|>",
+        add_prefix_space=False,
+        **kwargs
+    ):
+        super().__init__(
+            vocab_file,
+            merges_file,
+            tokenizer_file=tokenizer_file,
+            unk_token=unk_token,
+            bos_token=bos_token,
+            eos_token=eos_token,
+            add_prefix_space=add_prefix_space,
+            **kwargs,
+        )
+        SoftPrompt._register_tokenizer(self)
+
+
+class T5SPTokenizer(T5Tokenizer):
     def __init__(
         self,
         vocab_file,
